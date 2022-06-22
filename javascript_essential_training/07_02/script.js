93323f2df671167874c83acdb3f95e5e5bbfc1aa
Mennings <874491@fontys.nl>
2022-06-22 22:21:14 +0200

    js update

--- javascript_essential_training/07_02/script.js
+++ javascript_essential_training/07_02/script.js
@@ -6,5 +6,24 @@
 let item = "flashlight";
 
 const collection = ["Piggy", item, 5, true];
+const tester = ["boe"];
 
+console.log(collection[1]);
+
+collection[2]  = "Dimitrie";
+collection.push("mennings", 8, true) ;
+collection.unshift("eerste");
 console.log(collection);
+
+collection.forEach(function (item) {
+    item = `<li>${item}</li>`;
+    // console.log(item);
+
+});
+
+let baha = collection.find(function (item) {
+    if (item.length >= 5) {
+        console.log(item);
+    }
+})
+
